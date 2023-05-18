874dd2cf5fa9e710afab16f93a4b2d602202ea6a
Beatriz <beatriz.domingos@germinare.org.br>
2023-05-18 17:08:58 -0300

    teste4

--- src/main/java/Testes.java
+++ src/main/java/Testes.java
@@ -1,12 +1,13 @@
 public class Testes {
     public static void main(String[] args) {
         ConexaoPessoa conexaoPessoa = new ConexaoPessoa();
-
-        conexaoPessoa.inserir(108,"BEATRIZ BORTOLUCCI", 16, 1.58, 55, "feminino", "São Paulo - SP", 1, 1, 1, 1);
+//        conexaoPessoa.conectar();
+//        conexaoPessoa.inserir(108,"BEATRIZ BORTOLUCCI", 16, 1.58, 55, "feminino", "São Paulo - SP", 1, 1, 1, 1);
 //        conexaoPessoa.alterarIdadePesoAltura(17, 1.59, 56, 107);
 //        conexaoPessoa.alterarEstado("Minas Gerais - MG", 107);
 //        conexaoPessoa.remover(107);
 //        conexaoPessoa.consultar();
-
+//        conexaoPessoa.desconectar();
+        System.out.println("These texts are called functions that connect to a database.");
     }
 }
