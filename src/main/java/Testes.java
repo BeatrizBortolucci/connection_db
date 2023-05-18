2e891f997befd939c6a6b07533ba379f57e8b8a6
Beatriz <beatriz.domingos@germinare.org.br>
2023-05-18 17:04:25 -0300

    Teste

--- src/main/java/Testes.java
+++ src/main/java/Testes.java
@@ -1,12 +1,13 @@
 public class Testes {
     public static void main(String[] args) {
         ConexaoPessoa conexaoPessoa = new ConexaoPessoa();
-        conexaoPessoa.conectar();
-        conexaoPessoa.inserir(108,"BEATRIZ BORTOLUCCI", 16, 1.58, 55, "feminino", "São Paulo - SP", 1, 1, 1, 1);
-        conexaoPessoa.alterarIdadePesoAltura(17, 1.59, 56, 107);
-        conexaoPessoa.alterarEstado("Minas Gerais - MG", 107);
-        conexaoPessoa.remover(107);
-        conexaoPessoa.consultar();
-        conexaoPessoa.desconectar();
+//        conexaoPessoa.conectar();
+//        conexaoPessoa.inserir(108,"BEATRIZ BORTOLUCCI", 16, 1.58, 55, "feminino", "São Paulo - SP", 1, 1, 1, 1);
+//        conexaoPessoa.alterarIdadePesoAltura(17, 1.59, 56, 107);
+//        conexaoPessoa.alterarEstado("Minas Gerais - MG", 107);
+//        conexaoPessoa.remover(107);
+//        conexaoPessoa.consultar();
+//        conexaoPessoa.desconectar();
+        System.out.println("Hello World");
     }
 }
