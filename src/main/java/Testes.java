7fe49d02ccaf5b9075e58406327efe8162622903
Beatriz <beatriz.domingos@germinare.org.br>
2023-05-18 15:47:15 -0300

    Commit

---
+++ src/main/java/Testes.java
@@ -0,0 +1,12 @@
+public class Testes {
+    public static void main(String[] args) {
+        ConexaoPessoa conexaoPessoa = new ConexaoPessoa();
+        conexaoPessoa.conectar();
+        conexaoPessoa.inserir(107,"BEATRIZ BORTOLUCCI", 16, 1.58, 55, "feminino", "São Paulo - SP", 1, 1, 1, 1);
+        conexaoPessoa.alterarIdadePesoAltura(17, 1.59, 56, 107);
+        conexaoPessoa.alterarEstado("Minas Gerais - MG", 107);
+        conexaoPessoa.remover(107);
+        conexaoPessoa.consultar();
+        conexaoPessoa.desconectar();
+    }
+}
